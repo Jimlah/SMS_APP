7f4b05750a79bde04e56e307d3ee5c64e6a4b863
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-05-07 12:23:46 +0100

    chore: added file to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,13 @@
 
 namespace Database\Seeders;
 
-use App\Models\Autorities;
+use Faker\Factory;
+use App\Models\User;
+use App\Models\Admin;
 use App\Models\Student;
 use App\Models\Teacher;
-use App\Models\User;
-use Faker\Factory;
+use App\Models\Guardian;
+use App\Models\Autorities;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,14 +22,23 @@ class DatabaseSeeder extends Seeder
     {
         $faker = Factory::create();
 
-        User::create([
-            'username' => 'Zero',
-            'email' => 'abdullahij951@gmail.com',
-            'password' => "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi",
-            'email_verified_at' => now(),
-            'roles' => User::ROLE_SUPER_ADMIN
-        ]);
+        // User::create([
+        //     'username' => 'Zero',
+        //     'email' => 'abdullahij951@gmail.com',
+        //     'password' => "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi",
+        //     'email_verified_at' => now(),
+        //     'roles' => User::ROLE_ADMIN
+        // ]);
 
+        Admin::create([
+            'firstname' => 'abdullahi',
+            'lastname' => 'Jimoh',
+            'middlename' => 'Temidayo',
+            'email' => $faker->unique()->email,
+            'date_of_birth' => $faker->date(),
+            'phone_number' => $faker->phoneNumber
+        ]);
+        
         Student::create([
             'firstname' => 'abdullahi',
             'lastname' => 'Jimoh',
@@ -40,6 +51,16 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Teacher::create([
+            'firstname' => 'abdullahi',
+            'lastname' => 'Jimoh',
+            'middlename' => 'Temidayo',
+            'email' => $faker->unique()->email,
+            'date_of_birth' => $faker->date(),
+            'phone_number' => $faker->phoneNumber
+        ]);
+
+
+        Guardian::create([
             'firstname' => 'abdullahi',
             'lastname' => 'Jimoh',
             'middlename' => 'Temidayo',
