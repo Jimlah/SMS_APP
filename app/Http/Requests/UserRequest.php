81839f529ab1fe495419aaa4c1f41e2cde95c260
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-05-07 14:04:22 +0100

    chore: create request form for admin and Guardian

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class TeacherRequest extends FormRequest
+class UserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class TeacherRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return false;
     }
 
     /**
@@ -24,12 +24,10 @@ class TeacherRequest extends FormRequest
     public function rules()
     {
         return [
-            'firstname' => 'required|string',
-            'lastname' => 'required|string',
-            'middlename' => 'required|string',
-            'email' => 'required|string|email',
-            'date_of_birth' => 'date|nullable',
-            'phone_number' => 'required|string'
+            'username',
+            'email',
+            'password',
+            'roles',
         ];
     }
 }
